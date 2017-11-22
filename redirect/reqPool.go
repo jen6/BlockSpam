84003516f94f8a41f0244225fff5663cf656eee2
GeonSon <jen6@goodatlas.com>
2017-11-22 22:39:41 +0900

    add rwMutex in reqPool for threadsafe

--- redirect/reqPool.go
+++ redirect/reqPool.go
@@ -2,13 +2,18 @@ package spamreq
 
 import (
 	"github.com/jen6/BlockSpam/link"
+	"sync"
 )
 
 type RequestQueue struct {
+	rwMx  sync.RWMutex
 	queue []*link.Link
 }
 
-func (rq RequestQueue) IsEmpty() bool {
+func (rq *RequestQueue) IsEmpty() bool {
+	rq.rwMx.RLock()
+	defer rq.rwMx.RUnlock()
+
 	if len(rq.queue) == 0 {
 		return true
 	} else {
@@ -19,7 +24,9 @@ func (rq RequestQueue) IsEmpty() bool {
 func (rq *RequestQueue) Pop() *link.Link {
 	var ret *link.Link
 	if !rq.IsEmpty() {
+		rq.rwMx.Lock()
 		ret, rq.queue = rq.queue[0], rq.queue[1:]
+		rq.rwMx.Unlock()
 	} else {
 		ret = nil
 	}
@@ -27,5 +34,7 @@ func (rq *RequestQueue) Pop() *link.Link {
 }
 
 func (rq *RequestQueue) Push(link *link.Link) {
+	rq.rwMx.Lock()
+	defer rq.rwMx.Unlock()
 	rq.queue = append(rq.queue, link)
 }
