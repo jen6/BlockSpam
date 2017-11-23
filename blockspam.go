2ae28d7ba2a3b2f6dea73ec15ac4356a407890cb
GeonSon <jen6@goodatlas.com>
2017-11-23 12:52:27 +0900

    change blockspam not to return error

--- blockspam.go
+++ blockspam.go
@@ -10,16 +10,16 @@ import (
 	"sync"
 )
 
-func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) (bool, error) {
+func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) bool {
 	head := link.NewLinkHead(content)
 	reqQueue := spamreq.RequestQueue{}
 	reqQueue.Push(head)
 
 	numCpu := runtime.NumCPU()
 	var wg sync.WaitGroup
-	wg.Add(numCpu)
 
 	for i := 0; i < numCpu; i++ {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for {
@@ -30,8 +30,7 @@ func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) (boo
 				linkIter := reqQueue.Pop()
 				redirectionResult, err := spamreq.GetRedirectLinks(linkIter, redirectionDepth)
 				if err != nil {
-					fmt.Println(err)
-					return
+					continue
 				}
 				//if depth > max
 				if redirectionResult.LastResp == nil {
@@ -41,13 +40,12 @@ func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) (boo
 				statusCode := redirectionResult.LastResp.StatusCode
 				statusCode = statusCode - (statusCode % 100)
 				if statusCode == 300 {
-					return
+					continue
 				}
 
 				childLinks, err := spamparser.ParseLinks(redirectionResult)
 				if err != nil {
-					fmt.Println(err)
-					return
+					continue
 				}
 
 				for _, clink := range childLinks {
@@ -57,19 +55,15 @@ func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) (boo
 		}()
 	}
 	wg.Wait()
-	//TODO add error return in goroutine
 
 	resultLinks := head.FindDepth(redirectionDepth)
 	if len(resultLinks) == 0 {
-		return false, nil
+		return false
 	}
 	flag := false
 	for _, iterLink := range resultLinks {
-		domain, err := iterLink.GetDomain()
-		if err != nil {
-			return false, err
-		}
+		domain, _ := iterLink.GetDomain()
 		flag = flag || rabinkarp.Search(domain, spamLinkDomains)
 	}
-	return flag, nil
+	return flag
 }
