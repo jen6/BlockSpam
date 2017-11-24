53b4256029a3ce05c04f820fb29b6712ccdd1a0e
GeonSon <jen6@goodatlas.com>
2017-11-24 16:33:34 +0900

    change model to one goroutine for one link

--- blockspam.go
+++ blockspam.go
@@ -1,95 +1,25 @@
 package blockspam
 
 import (
-	"fmt"
 	"github.com/jen6/BlockSpam/link"
 	"github.com/jen6/BlockSpam/parser"
 	"github.com/jen6/BlockSpam/redirect"
 	"github.com/jen6/rabinkarp"
-	"runtime"
-	"sync"
 )
 
 func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) bool {
 	head := link.NewLinkHead(content)
-	reqQueue := make(chan *link.Link, 5000)
-	reqQueue <- head
+	goWait := make(chan interface{})
 
-	numCpu := runtime.NumCPU()
-	var wg sync.WaitGroup
-	var workDoneOnce, workWaitOnce sync.Once
-	goWait := make(chan struct{})
-
-	for i := 0; i < numCpu; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for {
-				var linkIter *link.Link
-				var ok bool
-				select {
-				case linkIter, ok = <-reqQueue:
-					if !ok {
-						return
-					}
-				default:
-					fmt.Println("novalue")
-					return
-				}
-
-				redirectionResult, err := spamreq.GetRedirectLinks(linkIter, redirectionDepth)
-				if err != nil {
-					workDoneOnce.Do(func() {
-						goWait <- struct{}{}
-						close(goWait)
-					})
-					continue
-				}
-				//if depth > max
-				if redirectionResult.LastResp == nil {
-					workDoneOnce.Do(func() {
-						goWait <- struct{}{}
-						close(goWait)
-					})
-					continue
-				}
-
-				statusCode := redirectionResult.LastResp.StatusCode
-				statusCode = statusCode - (statusCode % 100)
-				if statusCode == 300 {
-					workDoneOnce.Do(func() {
-						goWait <- struct{}{}
-						close(goWait)
-					})
-					continue
-				}
-
-				childLinks, err := spamparser.ParseLinks(redirectionResult)
-				if err != nil {
-					continue
-				}
-
-				for _, clink := range childLinks {
-					reqQueue <- clink
-				}
-
-				workDoneOnce.Do(func() {
-					goWait <- struct{}{}
-					close(goWait)
-				})
-			}
-		}()
-
-		workWaitOnce.Do(func() {
-			_ = <-goWait
-		})
-	}
-	wg.Wait()
+	go fetch(head, goWait, redirectionDepth)
+	<-goWait
+	close(goWait)
 
 	resultLinks := head.FindDepth(redirectionDepth)
 	if len(resultLinks) == 0 {
 		return false
 	}
+
 	flag := false
 	for _, iterLink := range resultLinks {
 		if iterLink.Depth > redirectionDepth {
@@ -99,4 +29,43 @@ func IsSpam(content string, spamLinkDomains []string, redirectionDepth int) bool
 		flag = flag || rabinkarp.Search(domain, spamLinkDomains)
 	}
 	return flag
+}
+
+func fetch(linkIter *link.Link, doneCh chan<- interface{}, redirectionDepth int) {
+	redirectionResult, err := spamreq.GetRedirectLinks(linkIter, redirectionDepth)
+	if err != nil {
+		doneCh <- struct{}{}
+		return
+	}
+	//if depth > max
+	if redirectionResult.LastResp == nil {
+		doneCh <- struct{}{}
+		return
+	}
+
+	statusCode := redirectionResult.LastResp.StatusCode
+	statusCode = statusCode - (statusCode % 100)
+	if statusCode == 300 {
+		doneCh <- struct{}{}
+		return
+	}
+
+	childLinks, err := spamparser.ParseLinks(redirectionResult)
+	if err != nil {
+		doneCh <- struct{}{}
+		return
+	}
+
+	resultChannels := make([]chan interface{}, len(childLinks))
+	for i, clink := range childLinks {
+		rch := make(chan interface{})
+		resultChannels[i] = rch
+		go fetch(clink, rch, redirectionDepth)
+	}
+
+	for _, rch := range resultChannels {
+		<-rch
+		close(rch)
+	}
+	doneCh <- struct{}{}
 }
